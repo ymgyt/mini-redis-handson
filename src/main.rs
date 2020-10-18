a067ca195678dc968caeadd29da614c61aace761
ymgyt <yamaguchi7073xtt@gmail.com>
2020-10-18 18:42:48 +0900

    Section: Spawning

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,45 @@ use mini_redis::{Connection, Frame};
 async fn main() {
     let mut listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("running...");
     loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        let (socket, addr) = listener.accept().await.unwrap();
+        println!("accept... {:?}", addr);
+
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
 async fn process(socket: TcpStream) {
-   let mut connection = Connection::new(socket);
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    println!("process");
 
-        let response = Frame::Error("unimplemented".to_string());
+    // a hashmap is used to store data.
+    let mut db = HashMap::new();
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
         connection.write_frame(&response).await.unwrap();
     }
 }
+
